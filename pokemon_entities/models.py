49f16a54b7b4bd34ca16c7b45a59d521b5d11303
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-07-29 21:16:43 +0300

    Added related name for link with previous evolution of pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,7 +11,13 @@ class Pokemon(models.Model):
     image = models.ImageField(upload_to='pokemons', null=True, blank=True)
     description = models.TextField(default='', blank=True)
 
-    next_evolution = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    next_evolution = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='previous_evolution',
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f'{self.title}'
