b5916c0e6fa1020e640a6eb82a7d21f1bc75f062
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-07-30 21:22:49 +0300

    Added verbose names for models fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, default='', blank=True)
-    title_jp = models.CharField(max_length=200, default='', blank=True)
+    title = models.CharField('название', max_length=200)
+    title_en = models.CharField('название (англ)', max_length=200, default='', blank=True)
+    title_jp = models.CharField('название (япон)', max_length=200, default='', blank=True)
 
-    image = models.ImageField(upload_to='pokemons', null=True, blank=True)
-    description = models.TextField(default='', blank=True)
+    image = models.ImageField('картинка', upload_to='pokemons', null=True, blank=True)
+    description = models.TextField('описание', default='', blank=True)
 
     previous_evolution = models.ForeignKey(
         'self',
+        verbose_name='из кого эволюционировал',
         related_name='next_evolution',
         on_delete=models.SET_NULL,
         null=True,
@@ -22,19 +23,23 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        verbose_name='покемон',
+    )
 
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField('широта')
+    longitude = models.FloatField('долгота')
 
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
+    appeared_at = models.DateTimeField('когда появится', null=True, blank=True)
+    disappeared_at = models.DateTimeField('когда исчезнет', null=True, blank=True)
 
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    strength = models.IntegerField(null=True, blank=True)
-    defence = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
+    level = models.IntegerField('уровень', null=True, blank=True)
+    health = models.IntegerField('здоровье', null=True, blank=True)
+    strength = models.IntegerField('сила', null=True, blank=True)
+    defence = models.IntegerField('защита', null=True, blank=True)
+    stamina = models.IntegerField('выносливость', null=True, blank=True)
 
     def __str__(self):
         return f'{self.pokemon} ({self.latitude}, {self.longitude})'
