f79effe53b5c1f891dbba6921ce16cbdc7dd506e
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-07-31 20:16:35 +0300

    Added registration of model for pokemon element type in admin interface

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Pokemon, PokemonEntity
+from .models import Pokemon, PokemonEntity, PokemonElementType
 
 
 admin.site.register(Pokemon)
 
 admin.site.register(PokemonEntity)
+
+admin.site.register(PokemonElementType)
