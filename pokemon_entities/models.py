a02cd0f651c023aff698677524c9b843a2f516f1
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-08-01 19:08:20 +0300

    Added field for store icon of pokemon element type

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class PokemonElementType(models.Model):
     title = models.CharField('название', max_length=200)
+    image = models.ImageField('значок', upload_to='element_types', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
